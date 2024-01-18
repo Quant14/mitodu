cb600d376150defda69915c78b327ec5ffdf5710
grapeqna <yliychova@gmail.com>
2024-01-18 16:47:46 +0200

    GlobalStylesProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Sidebar from './components/sidebar/sidebar'
+import GlobalStyleProvider from './providers/GlobalStylesProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Sidebar />
-        {children}</body>
+        <GlobalStyleProvider>
+          <Sidebar />
+          {children}
+        </GlobalStyleProvider>
+      </body>
     </html>
   )
 }
---
+++ app/providers/GlobalStylesProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+interface Props{
+    children: React.ReactNode
+}
+
+function GlobalStyleProvider( {children}: Props){
+    return <GlobalStyles>{children}</GlobalStyles>;
+}
+
+const GlobalStyles = styled.div``;
+
+export default GlobalStyleProvider
